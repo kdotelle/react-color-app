b72adee01d4ce5a666787736e92e04a2a4ab3972
Kristen <lloyd.kristen91@gmail.com>
2021-08-27 19:50:04 -0400

    added palette

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        <div className="Palette-colors">{/* bunch of color boxes */}</div>
+      </div>
+    );
+  }
+}
+
+export default Palette;
