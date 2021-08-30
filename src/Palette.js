d8492bdeb1dbcbfda710167674aa0d596b746d7b
Kristen <lloyd.kristen91@gmail.com>
2021-08-30 10:33:24 -0400

    color boxes added

--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
 
 class Palette extends Component {
   render() {
+    const colorBoxes = this.props.colors.map((color) => (
+      <ColorBox background={color.color} name={color.name} />
+    ));
     return (
       <div className="Palette">
-        <div className="Palette-colors">{/* bunch of color boxes */}</div>
+        <div className="Palette-colors">{colorBoxes}</div>
       </div>
     );
   }
