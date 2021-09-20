7c8420470b677a667bb10f95b814a94556631fe1
Kristen <lloyd.kristen91@gmail.com>
2021-09-20 16:43:06 -0400

    add svg background to palette list

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,12 +1,20 @@
 import sizes from "./sizes";
+import bg from "./bg.svg";
 
 const styles = {
   root: {
-    backgroundColor: "blue",
+    //backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center",
+    /* background by SVGBackgrounds.com */
+    backgroundColor: "#0535B5",
+    backgroundImage: `url(${bg})`,
+    overflow: "auto",
+  },
+  heading: {
+    fontSize: "2rem",
   },
   container: {
     width: "50%",
@@ -42,6 +50,7 @@ const styles = {
     },
     [sizes.down("xs")]: {
       gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1%",
     },
   },
 };
